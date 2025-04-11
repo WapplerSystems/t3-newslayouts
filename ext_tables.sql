60251622008537fbc6a6a791b59c64dc3d23b13b
Sven Wappler <wappler@wappler.systems>
2025-04-11 16:14:01 +0200

    updates

--- ext_tables.sql
+++ ext_tables.sql
@@ -6,3 +6,8 @@ CREATE TABLE tx_news_domain_model_news
 	layout  varchar(50)      DEFAULT '' NOT NULL,
 	gallery int(11) unsigned DEFAULT '0'
 );
+
+CREATE TABLE tt_content
+(
+	tx_newslayouts_dummyimage int(11) unsigned DEFAULT '0'
+);
